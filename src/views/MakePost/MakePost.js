a91ac41f1a821ba569dc7519b0c46085bf4318d8
Sreehari <sreehari.rammohan@gmail.com>
2020-12-19 13:50:26 -0800

    Update MakePost.js

--- src/views/MakePost/MakePost.js
+++ src/views/MakePost/MakePost.js
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "components/CustomNavbar/CustomNavbar.js"
 
-import { TextField, Button } from "@material-ui/core"
+import { TextField, Select, Button } from "@material-ui/core"
+import {useSelector} from "react-redux"
+import {API_URL} from "../../Redux/constants"
+
+const axios = require("axios")
 
 function MakePost() {
 
@@ -9,12 +13,26 @@ function MakePost() {
     const [clip, setClip] = useState("")
     const [thoughts, setThoughts] = useState("")
 
+    const [clips, setClips] = useState([])
+    
+    const mongo_id = useSelector(state => state.mongo_id)
 
     function post() {
         console.log(postTitle, clip, thoughts)
 
         //axios post to backend
     }
+
+    useEffect(() => {
+        const payload = {
+            "mongo_id": mongo_id
+        }
+        axios.get(API_URL + "/clip", payload)
+            .then(res => {
+                console.log(res.data)
+                setClips(res.data.clip_names)
+            })
+    }, [])
 
     return (
         <React.Fragment>
@@ -24,9 +42,13 @@ function MakePost() {
                     <form noValidate autoComplete="off">
                         <p>Make Post</p>
 
-                        <TextField className="mt-3" fullWidth id="standard-basic" label="Post Title" value={post} onClick={(e) => {setPostTitle(e.target.value)}}/>
-                        <TextField className="mt-3" fullWidth id="standard-basic" label="Select Clip from Library" value={clip} onClick={(e) => {setClip(e.target.value)}} />
-                        <TextField className="mt-3" multiline fullWidth id="standard-basic" label="Thoughts" value={thoughts} onClick={(e) => {setThoughts(e.target.value)}} />
+                        <TextField className="mt-3" fullWidth id="standard-basic" label="Post Title" value={post} onClick={(e) => { setPostTitle(e.target.value) }} />
+                        <Select className="mt-3" fullWidth id="standard-basic" label="Select Clip from Library" value={clip} onClick={(e) => { setClip(e.target.value) }}>
+                            <option value={10}>Ten</option>
+                            <option value={20}>Twenty</option>
+                            <option value={30}>Thirty</option>
+                        </Select>
+                        <TextField className="mt-3" multiline fullWidth id="standard-basic" label="Thoughts" value={thoughts} onClick={(e) => { setThoughts(e.target.value) }} />
                         <Button className="mt-3" variant="outlined" color="primary">
                             Post
                         </Button>
